ec6eba61b5289491047c1ff4683cac074ae9a9ad
Socrates Palacios Moreno <socsofteng@gmail.com>
2024-12-23 00:05:52 -0500

    Backend terminado, además se tiene la carpeta para el front y el analisis de datos

---
+++ Reports/analizador_datos.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+
